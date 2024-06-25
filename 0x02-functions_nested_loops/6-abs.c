0684704292c1e3e9736c17b9373f65d78e7e32e7
Nixon <nixusmwas@gmail.com>
2024-06-26 02:05:54 +0300

    Finding the absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
  * _abs - function to find the absolute value of an int
- * int: takesa an integer as its parameter
+ * @d: takesa an integer as its parameter
  * Return: returns an integer
  */
-int _abs(int)
+int _abs(int d)
 {
-	/*int n;*/
 
-	if (int n >= 1)
+	if (int d >= 1)
 	{
-		int result = n;
+		int result = d;
 
 		return (result);
 	}
-	else if (int n == 0)
+	else if (int d == 0)
 	{
 		int result = 0;
 
 		return (result);
 	}
-	else if (int n >= -1)
+	else if (int d >= -1)
 	{
-		int result = -n;
+		int result = -d;
 
 		return (result);
 	}

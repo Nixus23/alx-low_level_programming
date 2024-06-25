792fc89fb095c447b11dbac03e129fc5f5371005
Nixon <nixusmwas@gmail.com>
2024-06-25 15:27:58 +0300

    printing the alphabet on a user function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - prints the alphabet in lower case
+ *
+ * Return: no return type as it is void
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}

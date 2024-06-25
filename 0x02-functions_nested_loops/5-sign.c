dc7dff05901d02332088b2e09010eeeb0536f430
Nixon <nixusmwas@gmail.com>
2024-06-26 00:43:41 +0300

    check number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - function to output the sign of an integer
+ * @n : function argument
+ * Return: returns an integer depending on the value
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+
+}

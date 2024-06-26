476bc3343d107398276abf41a6523a295ba24dd4
Nixon <nixusmwas@gmail.com>
2024-06-26 19:37:34 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@ int print_last_digit(int a)
 {
 	int last;
 
-	if (a < 0)
-	{
-		a = a * -1;
-	}
-	last = (a % 10);
+	last = a % 10;
 
+	if (a <= -1)
+	{
+		last = last * -1;
+	}
 	_putchar('0' + last);
 	return (last);
 

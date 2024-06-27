d1da5926a0f241674ceabfee436db722d23d2fb1
Nixon <nixusmwas@gmail.com>
2024-06-27 19:02:26 +0300

    print single digit combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,17 @@ int main(void)
 {
 	int num = 0;
 
-	while (num <= 8)
+	while (num <= 9)
 	{
 		putchar('0' + num);
+
+		if (num == 9)
+			break;
+
 		putchar(',');
 		putchar(' ');
 		num++;
 	}
-	putchar('0' + 9);
+	putchar('\n');
 	return (0);
 }

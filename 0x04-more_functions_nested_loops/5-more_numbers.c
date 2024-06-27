33f1b7b500632b82de0b07e2c3015df659e8c19f
Nixon <nixusmwas@gmail.com>
2024-06-27 17:48:16 +0300

    print numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - function prints 0 to 14 ten times
+ * function has no arguments
+ * function has a void return
+ */
+void more_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		int b = 0;
+
+		while (b <= 14)
+		{
+			_putchar('0' + b);
+			b++;
+		}
+		_putchar('\n');
+	}
+}

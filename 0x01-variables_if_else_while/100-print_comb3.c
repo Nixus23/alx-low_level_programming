e30e47dc788d5b35c47678c547f9b761035a7841
Nixon <nixusmwas@gmail.com>
2024-06-27 20:06:06 +0300

    two digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * function prints different possible combinations of two digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0, m;
+
+	while (n <= 9)
+	{
+		m = n + 1;
+		while (m < 10)
+		{
+			putchar('0'+ n);
+			putchar('0' + m);
+			putchar(',');
+			putchar(' ');
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

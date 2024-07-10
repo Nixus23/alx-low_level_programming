a72f47efcb40b1893cfa1bc18b3be25334e59922
Nixon <nixusmwas@gmail.com>
2024-07-11 00:08:02 +0300

    the power of recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - function returns x raised to the power of y
+ * @x: int x, value to raise
+ * @y: int y, int value to power by
+ * Return: The function returns an int value
+ */
+int _pow_recursion(int x, int y)
+{
+	int sol;
+
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+
+	if (y != 0)
+	{
+		sol = (x * _pow_recursion (x, y - 1));
+	}
+	
+	return (sol);
+}

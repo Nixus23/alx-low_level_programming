80f5349720964f48469977e03c50b0c247f74771
Nixon <nixusmwas@gmail.com>
2024-07-13 11:11:21 +0300

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,8 @@
  * @argv: function parameter to store the passed string arguments
  * Return: Always 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }

3ae11e049a79a8aa3ac06b8d07625e9bc69d80d5
Nixon <nixusmwas@gmail.com>
2024-07-20 09:23:50 +0300

    add numbers as main arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,4 +8,25 @@
  */
 int main(int argc, char *argv[])
 {
+	int sum = 0, row, column;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+
+	for (column = 1; column < argc; column++)
+	{
+		for (row = 0; argv[column][row]; row++)
+		{
+			if (argv[column][row] < '0' || argv[column][row] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[column]);
+	}
+	printf("%i\n", sum);
+	return (0);
 }

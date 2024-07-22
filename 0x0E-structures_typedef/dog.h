ff55e77cc3ad09be3a7bd5497220669c554c47ef
Nixon <nixusmwas@gmail.com>
2024-07-23 02:46:00 +0300

    the struct dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - has dog attributes
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

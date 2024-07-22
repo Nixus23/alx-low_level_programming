c25742a102a050672c5b9344c66fc71e3c650462
Nixon <nixusmwas@gmail.com>
2024-07-22 22:53:16 +0300

    file standard macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function entry point
+ * @void: function has no parameters
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

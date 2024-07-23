0eb133673c8ae850563061622c8ae8910cf424ed
Nixon <nixusmwas@gmail.com>
2024-07-23 12:25:12 +0300

    a function that initializes a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - function initializes a struct
+ * * @d: a pointer to the struct
+ * @name: the pointer to the name of the dog in the struct
+ * @age: the age of the dog in the struct
+ * @owner: the pointer to the name of the owner of the dog
+ * Return: the function has a void return
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
